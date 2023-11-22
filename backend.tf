b63b7b8cf8d3fcbfa3b2a95d53c3aedfba346a59
sanjay <ksanjay20009@gmail.com>
2023-11-22 13:38:40 +0530

    updates

---
+++ backend.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.81.0"
+    }
+  }
+
+ backend "azurerm" {
+    resource_group_name  = "sanjay-devops-rg"
+    storage_account_name = "sanjaystac01"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
+}
+
+
