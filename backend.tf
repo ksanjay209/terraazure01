234ac5fc20ebce0713c4e9bddd42999f0618278a
ksanjay209 <66907976+ksanjay209@users.noreply.github.com>
2023-11-22 15:54:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -16,10 +16,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
-  # tenant_id       = var.tenant_id
+  subscription_id = var.MVP_SUBSCRIPTION
+  client_id       = var.AZURE_CLIENT_ID
+  client_secret   = var.AZURE_CLIENT_SECRET
+  tenant_id       = var.AZURE_TENANT_ID
 }
+
 
 
