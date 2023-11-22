d6d4bac9bc13696cd16259d487afcac7509b5c24
ksanjay209 <66907976+ksanjay209@users.noreply.github.com>
2023-11-22 22:08:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,13 +8,3 @@ variable "ResourceGroup" {
     default     = "rg-connectivity-network-001"   
 }
 
- variable "AZURE_CLIENT_ID" {
-    type        = string
-}
-
-variable "AZURE_CLIENT_SECRET" {
-}
-variable "AZURE_TENANT_ID" {
-}
-variable "MVP_SUBSCRIPTION" {
-}
