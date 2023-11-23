49e8a627d3159acf7669d434ab7a72f80726b576
sanjay <ksanjay20009@gmail.com>
2023-11-23 12:57:23 +0530

    added map for resource groups

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
+/*
 resource "azurerm_resource_group" "resourcegroups" {
     name        = var.ResourceGroup
     location    = var.Location
+}
+*/
+resource "azurerm_resource_group" "resourcegroups" {
+
+  for_each = var.ResourceGroups
+  name = each.value
+  location = var.Location
+
 }
--- variables.tf
+++ variables.tf
@@ -2,9 +2,21 @@ variable "Location" {
     type        = string
     default     = "WestEurope"  
 }
-
+/*
 variable "ResourceGroup" {
     type        = string
     default     = "sanjay-rg"   
 }
-
+*/
+variable "ResourceGroups" {
+  type    = map
+  default = {
+    "HubNetwork"          = "Terraform-Hub-Network"
+    "HubCoreVMs"          = "Terraform-Hub-CoreVMs"
+    "HubSharedServices"   = "Terraform-Hub-SharedServices" 
+    "SpokeNetwork"        = "Terraform-Spoke-Network"
+    "SpokeSharedServices" = "Terraform-Spoke-SharedServices"
+    "SpokeCitrixCore"     = "Terraform-Spoke-Citrix-Core"
+    "SpokeCitrixSilo1"    = "Terraform-Spoke-Citrix-Silo1"
+  }
+}
